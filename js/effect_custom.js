2f005b6e8f544c6e3689ec85e83e542ba1ee894e
Kirr <rongnhat2@gmail.com>
2020-04-20 12:22:26 +0700

    edit feedback v_01

--- js/effect_custom.js
+++ js/effect_custom.js
@@ -6,14 +6,42 @@ $('.tab_select').on('click', function(){
 	$('.tab_select').eq(index).addClass('is-select');
 })
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    items:3,
-})
-$('.I-carousel').find('.next_carousel').on('click', function(){
-    $('.slider_carousel').trigger('next.owl.carousel');
-})
-$('.I-carousel').find('.prev_carousel').on('click', function(){
-    $('.slider_carousel').trigger('prev.owl.carousel');
+$('.side_item').on('click', function(){
+	var index = $(this).index()
+	$('.side_item').removeClass('is-select');
+	$('.side_item').eq(index).addClass('is-select');
+	$('.content_banner_wrapper').removeClass('is-select');
+	$('.content_banner_wrapper').eq(index).addClass('is-select');
 })
 
+var stickyNav = $('.item_side').offset().top;
+window.onscroll = function() {
+	NavFixed();
+}
+function NavFixed() {
+
+	var valueY = window.pageYOffset - stickyNav
+	var maxY = $('.item_list').height() - $('.I-preview').height()
+	$('.item_side').css({
+		'height': $('.item_list').height() + 'px'
+	})
+  	if (valueY > 0) {
+  		if(valueY > maxY){
+	  		$('.item_side').find('.I-preview').css({
+	  			'position': 'absolute',
+	  			'top': 'auto',
+	  			'bottom': 0
+	  		})
+  		}else{
+	  		$('.item_side').find('.I-preview').css({
+	  			'position': 'fixed',
+	  			'top': 0,
+	  			'bottom': 'auto',
+	  		})
+  		}
+  	} else {
+  		$('.item_side').find('.I-preview').css({
+  			'position': 'relative',
+  		})
+  	}
+}
